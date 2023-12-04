3eab55d125abdee76f2b10d2cd58baf80625896c
Nbiba Bedis <bedisnbiba@gmail.com>
2023-12-04 18:02:01 +0100

    0.2.8

--- main.ts
+++ main.ts
@@ -77,7 +77,7 @@ class MainWindow extends Gtk.ApplicationWindow {
     this.#about.set_license_type(Gtk.License.MIT_X11);
     this.#about.set_website("https://github.com/sigmaSd/gnome-nosleep");
     this.#about.set_website_label("Github");
-    this.#about.set_version("0.2.7");
+    this.#about.set_version("0.2.8");
     this.#about.set_logo_icon_name("io.github.sigmasd.nosleep");
 
     this.#about.set_visible(true);
