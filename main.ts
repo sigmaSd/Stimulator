3a86efe636b807b56bba204f37e00902827bbf41
Nbiba Bedis <bedisnbiba@gmail.com>
2023-12-21 23:01:48 +0100

    rebase

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ import {
   kw,
   NamedArgument,
   python,
-} from "https://raw.githubusercontent.com/sigmaSd/deno-gtk-py/0.2.1/mod.ts";
+} from "https://raw.githubusercontent.com/sigmaSd/deno-gtk-py/0.2.2/mod.ts";
 import { t } from "./i18n.ts";
 
 const VERSION = "0.4.1";
