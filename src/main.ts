a44000bb30e137effe423e685a18793e249a13cc
Nbiba Bedis <bedisnbiba@gmail.com>
2024-01-12 11:49:01 +0100

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S  deno run --allow-read=locales --allow-ffi --allow-env=DENO_PYTHON_PATH,CSS --unstable-ffi main.ts
+#!/usr/bin/env -S  deno run --allow-read=./src/locales --allow-ffi --allow-env=DENO_PYTHON_PATH,CSS --unstable-ffi
 import {
   Adw,
   Adw_,
