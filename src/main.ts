3853b54e1912d3d504b045d6b7bd3f3ef55df9a5
Nbiba Bedis <bedisnbiba@gmail.com>
2024-01-14 11:47:00 +0100

    0.7.1

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import {
 } from "https://raw.githubusercontent.com/sigmaSd/deno-gtk-py/0.2.10/mod.ts";
 import { systemLocale, t } from "./i18n.ts";
 
-const VERSION = "0.7.0";
+const VERSION = "0.7.1";
 
 interface Flags {
   "logout"?: number;
