2d59a10e317c4180466fbd7db3c4f502e951f831
Nbiba Bedis <bedisnbiba@gmail.com>
2024-01-25 18:28:42 +0100

    upgrade bot to ultra mode

--- scripts/compileTranslations.ts
+++ scripts/compileTranslations.ts
@@ -6,15 +6,17 @@ import i18n, { i18next } from "../src/i18n.ts";
 async function genTranslations() {
   for await (const lang of Deno.readDir("./po")) {
     const langName = lang.name.slice(0, -3);
+    const poFilePath = "./po/" + lang.name;
     const compiled = await gettextToI18next(
       langName,
-      await Deno.readTextFile("./po/" + lang.name),
-    );
+      await Deno.readTextFile(poFilePath),
+    ).then((data: string) => JSON.parse(data));
+    const verified = verifyAndFixPoFiles(poFilePath, compiled);
     const targetDir = "./src/locales/" + langName;
     await Deno.mkdir(targetDir, { recursive: true });
     await Deno.writeTextFile(
       targetDir + "/translation.json",
-      compiled,
+      JSON.stringify(verified),
     );
   }
 }
@@ -56,4 +58,46 @@ Categories=Utility;
 if (import.meta.main) {
   await genTranslations();
   await genDesktopFile();
+  await denoFmt();
+}
+
+// deno-lint-ignore no-explicit-any
+function verifyAndFixPoFiles(poFilePath: string, compiled: any) {
+  const compiledEntries = Object.entries(compiled);
+  let changes = false;
+  Object.values(EN_UI_LABELS).forEach((prop, index) => {
+    // 0 is the msgid
+    // 1 is the msgstr
+    if (compiledEntries[index][0] !== prop) {
+      compiledEntries[index][0] = prop;
+      compiledEntries[index][1] = "";
+      changes = true;
+    }
+  });
+  if (changes) {
+    Deno.writeTextFileSync(
+      poFilePath,
+      `\
+msgid ""
+msgstr ""
+"Content-Type: text/plain; charset=UTF-8\\n"
+
+${
+        compiledEntries.map(([msgid, msgstr]) =>
+          `\
+msgid "${msgid}"
+msgstr "${msgstr}"
+`
+        ).join("\n")
+      }`,
+    );
+  }
+  return Object.fromEntries(compiledEntries);
+}
+
+async function denoFmt() {
+  await new Deno.Command("deno", {
+    args: ["fmt", "--indent-width", "4", "./src/locales/"],
+  }).spawn()
+    .status;
 }
