bd5806f61b2539ec58d249b7dd34e4804dc7557d
Nbiba Bedis <bedisnbiba@gmail.com>
2024-02-01 16:58:25 +0100

    update deno gtk

--- vendor/raw.githubusercontent.com/#sigmasd_6a50d/deno-gtk-py/0.4.1/mod.ts
+++ vendor/raw.githubusercontent.com/#sigmasd_6a50d/deno-gtk-py/0.4.1/mod.ts
@@ -7,3 +7,4 @@ export * as Gdk4_ from "./src/gdk4.ts";
 export * as Gio2_ from "./src/gio2.ts";
 export * as GLib2_ from "./src/glib2.ts";
 export * as GObject2_ from "./src/gobject2.ts";
+export * as AppIndicator3 from "./src/appindicator3.ts";
