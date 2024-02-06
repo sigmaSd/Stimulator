ac24fdff07d20f1ecd3b4f19c17e0bf603cd452d
Nbiba Bedis <bedisnbiba@gmail.com>
2024-02-06 19:08:53 +0100

    unlock metaprogramming in js

--- src/consts.ts
+++ src/consts.ts
@@ -1,71 +1,50 @@
 import { t } from "./i18n.ts";
 
-export const EN_UI_LABELS = {
-  AppName: "Stimulator",
-  Comments: "Keep your computer awake",
-  Keywords: "caffeine;nosleep;awake;keepawake;keepon;",
-  SuspendTitle: "Disable Automatic Suspending",
-  IdleTitle: "Disable Screen Blanking and Locking",
-  SystemDefault: "Current state: System default",
-  Indefinitely: "Current state: Indefinitely",
-  SimulatorActive: "Stimulator is active",
-  ConfirmClose: "Close Stimulator?",
-  ConfirmCloseBody: "Stimulator is active, do you want to close it?",
-  Cancel: "Cancel",
-  Close: "Close",
-  Preferences: "Preferences",
-  Theme: "Theme",
-  ThemeSystem: "System Theme",
-  ThemeLight: "Light",
-  ThemeDark: "Dark",
-  EnableExistConfirmation: "Closing Confirmation",
-  EnableExistConfirmationSubTitle:
-    "Ask for confirmation to close when Stimulator is active",
-  KeyboardShortcuts: "Keyboard Shortcuts",
-  General: "General",
-  MainMenu: "Open Menu",
-  Quit: "Quit",
-  About: "About Stimulator",
-  UnsupportedSystem: "Unsupported System",
-  UnsupportedSystemBody:
-    "Your desktop environment doesn't support Stimulator, click close to quit",
+export class EN_UI_LABELS {
+  static AppName = "Stimulator";
+  static Comments = "Keep your computer awake";
+  static Keywords = "caffeine;nosleep;awake;keepawake;keepon;";
+  static SuspendTitle = "Disable Automatic Suspending";
+  static IdleTitle = "Disable Screen Blanking and Locking";
+  static SystemDefault = "Current state= System default";
+  static Indefinitely = "Current state= Indefinitely";
+  static SimulatorActive = "Stimulator is active";
+  static ConfirmClose = "Close Stimulator?";
+  static ConfirmCloseBody = "Stimulator is active; do you want to close it?";
+  static Cancel = "Cancel";
+  static Close = "Close";
+  static Preferences = "Preferences";
+  static Theme = "Theme";
+  static ThemeSystem = "System Theme";
+  static ThemeLight = "Light";
+  static ThemeDark = "Dark";
+  static EnableExistConfirmation = "Closing Confirmation";
+  static EnableExistConfirmationSubTitle =
+    "Ask for confirmation to close when Stimulator is active";
+  static KeyboardShortcuts = "Keyboard Shortcuts";
+  static General = "General";
+  static MainMenu = "Open Menu";
+  static Quit = "Quit";
+  static About = "About Stimulator";
+  static UnsupportedSystem = "Unsupported System";
+  static UnsupportedSystemBody =
+    "Your desktop environment doesn't support Stimulator; click close to quit";
   // we don't advertise tray icon support
-  EnableTrayIcon: "Run in Background",
-  Show: "Show",
-};
+  static EnableTrayIcon = "Run in Background";
+  static Show = "Show";
+}
 
-export const UI_LABELS = {
-  AppName: t(EN_UI_LABELS.AppName),
-  Comments: t(EN_UI_LABELS.Comments),
-  Keywords: t(EN_UI_LABELS.Keywords),
-  SuspendTitle: t(EN_UI_LABELS.SuspendTitle),
-  IdleTitle: t(EN_UI_LABELS.IdleTitle),
-  SystemDefault: t(EN_UI_LABELS.SystemDefault),
-  Indefinitely: t(EN_UI_LABELS.Indefinitely),
-  SimulatorActive: t(EN_UI_LABELS.SimulatorActive),
-  ConfirmClose: t(EN_UI_LABELS.ConfirmClose),
-  ConfirmCloseBody: t(EN_UI_LABELS.ConfirmCloseBody),
-  Cancel: t(EN_UI_LABELS.Cancel),
-  Close: t(EN_UI_LABELS.Close),
-  Preferences: t(EN_UI_LABELS.Preferences),
-  Theme: t(EN_UI_LABELS.Theme),
-  ThemeSystem: t(EN_UI_LABELS.ThemeSystem),
-  ThemeLight: t(EN_UI_LABELS.ThemeLight),
-  ThemeDark: t(EN_UI_LABELS.ThemeDark),
-  EnableExistConfirmation: t(EN_UI_LABELS.EnableExistConfirmation),
-  EnableExistConfirmationSubTitle: t(
-    EN_UI_LABELS.EnableExistConfirmationSubTitle,
-  ),
-  KeyboardShortcuts: t(EN_UI_LABELS.KeyboardShortcuts),
-  General: t(EN_UI_LABELS.General),
-  MainMenu: t(EN_UI_LABELS.MainMenu),
-  Quit: t(EN_UI_LABELS.Quit),
-  About: t(EN_UI_LABELS.About),
-  UnsupportedSystem: t(EN_UI_LABELS.UnsupportedSystem),
-  UnsupportedSystemBody: t(EN_UI_LABELS.UnsupportedSystemBody),
-  EnableTrayIcon: t(EN_UI_LABELS.EnableTrayIcon),
-  Show: t(EN_UI_LABELS.Show),
-};
+@translate
+export class UI_LABELS extends EN_UI_LABELS {}
+
+// deno-lint-ignore no-explicit-any
+function translate<T extends { [key: string]: any }>(klass: T, _: any) {
+  for (const prop in klass) {
+    // deno-lint-ignore no-explicit-any
+    (klass as any)[prop] = t((klass as any)[prop]);
+  }
+  return klass;
+}
 
 export const APP_ID = "io.github.sigmasd.stimulator";
 export const APP_NAME = UI_LABELS.AppName;
