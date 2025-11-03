06edb2db6c8b47b87c9f68cc524f2d308a8f6dc1
Nbiba Bedis <bedisnbiba@gmail.com>
2025-11-03 17:39:29 +0100

    1.13.2

--- src/consts.ts
+++ src/consts.ts
@@ -59,7 +59,7 @@ export class UI_LABELS extends EN_UI_LABELS {}
 
 export const APP_ID = "io.github.sigmasd.stimulator";
 export const APP_NAME = UI_LABELS.Stimulator;
-export const VERSION = "1.13.1";
+export const VERSION = "1.13.2";
 
 function genLabels<T>(klass: T, ctx: ClassDecoratorContext) {
   ctx.addInitializer(function () {
